9d4dc96ce469149b482f3b5950d7fbab6f3fc4b0
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2020-02-12 17:42:55 +0100

    implemented stage 1

---
+++ Linear Equations Solver/task/src/solver/Main.java
@@ -0,0 +1,11 @@
+package solver;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        System.out.println(b / a);
+    }
+}
---
+++ Linear Equations Solver/task/test/SolverTest.java
@@ -0,0 +1,74 @@
+import org.hyperskill.hstest.v6.testcase.CheckResult;
+import org.hyperskill.hstest.v6.stage.BaseStageTest;
+import org.hyperskill.hstest.v6.testcase.TestCase;
+import solver.Main;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+
+public class SolverTest extends BaseStageTest<String> {
+
+    public SolverTest() throws Exception {
+        super(Main.class);
+    }
+
+    @Override
+    public List<TestCase<String>> generate() {
+        List<TestCase<String>> tests = List.of(
+            new TestCase<String>()
+                .setInput("4 5 7\n3 9 9"),
+            new TestCase<String>()
+                .setInput("1 2 3\n4 5 6"),
+            new TestCase<String>()
+                .setInput("1.2 3.4 5.6\n7.8 9.0 12.3"),
+            new TestCase<String>()
+                .setInput("23.21 32.12 65.43\n1 0 1")
+        );
+
+        for (var test : tests) {
+            test.setAttach(solve(test.getInput()));
+        }
+
+        return tests;
+    }
+
+    private String solve(String input) {
+        Scanner sc = new Scanner(input);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double d = sc.nextDouble();
+        double e = sc.nextDouble();
+        double f = sc.nextDouble();
+
+        double y = (f - c * (d / a)) / (e - b * (d / a));
+        double x = (c - b * y) / a;
+
+        return x + " " + y;
+    }
+
+    @Override
+    public CheckResult check(String reply, String clue) {
+        try {
+            double[] actual = Arrays.stream(reply.split(" ")).mapToDouble(Double::parseDouble).toArray();
+            double[] expected = Arrays.stream(clue.split(" ")).mapToDouble(Double::parseDouble).toArray();
+            if (actual.length != expected.length) {
+                return CheckResult.FALSE;
+            }
+            for (int i = 0; i < actual.length; i++) {
+                if (abs(actual[i] - expected[i]) > 0.001) {
+                    return CheckResult.FALSE;
+                }
+            }
+            return CheckResult.TRUE;
+        }
+        catch (NumberFormatException ex) {
+            return new CheckResult(false, "Can't parse numbers - check if you don't print any additional symbols except a space between the numbers");
+        }
+    }
+}
